a225bb24e53bc96a05ea7d269542b2944ddde325
김재환 <fodore12@gmail.com>
2025-02-24 16:52:56 +0900

    feat: 회원가입 페이지 작업중

---
+++ app/(logged-out)/sign-up/page.tsx
@@ -0,0 +1,190 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { PersonStandingIcon } from 'lucide-react';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+const formSchema = z
+  .object({
+    email: z.string().email(),
+    accountType: z.enum(['personal', 'company']),
+    companyName: z.string().optional(),
+    numberOfEmployees: z.coerce.number().optional(),
+    dob: z.date().refine((date) => {
+      const today = new Date();
+      // const eighteedYearsAgo = new Date(
+      //   today.getFullYear() - 18,
+      //   today.getMonth(),
+      //   today.getDate()
+      // );
+      // return date <= eighteedYearsAgo;
+      return date.getFullYear() <= today.getFullYear() - 18;
+    }, 'You must beat least 18 years old'),
+  })
+  .superRefine((data, context) => {
+    if (data.accountType === 'company' && !data.companyName) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['companyName'],
+        message: 'Company name is required',
+      });
+    }
+
+    if (
+      data.accountType === 'company' &&
+      (!data.numberOfEmployees || data.numberOfEmployees < 1)
+    ) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['numberOfEmployees'],
+        message: 'Number of employees is required',
+      });
+    }
+  });
+
+export default function SignupPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  const handleSubmit = () => {
+    console.log('login validation passed');
+  };
+
+  const accountType = form.watch('accountType');
+
+  return (
+    <>
+      <PersonStandingIcon size={50} />
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Sign up</CardTitle>
+          <CardDescription>Sign up for a new SupportMe account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={form.handleSubmit(handleSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="John@doe.com"
+                        type="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="accountType"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Account Type</FormLabel>
+                    <Select onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select an account type" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="personal">Personal</SelectItem>
+                        <SelectItem value="company">Company</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {accountType === 'company' && (
+                <>
+                  <FormField
+                    control={form.control}
+                    name="companyName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Company name</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Company name"
+                            type="text"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="numberOfEmployees"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Employees</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Employees"
+                            type="number"
+                            min={0}
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </>
+              )}
+              <Button type="submit">Sign up</Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="justify-between">
+          <small>Already have an account?</small>
+          <Button asChild variant="outline" size="sm">
+            <Link href="/login">Login</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
