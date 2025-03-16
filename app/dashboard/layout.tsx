aa515c594b5716f4fb2911f24ae6f06423455122
김재환 <kimjaehwan@gimjaehwan-ui-MacBookPro.local>
2025-03-16 13:38:04 +0900

    feat: 모바일에서 좌측 내비게이션 없애기

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import MainMenu from "./components/main-menu";
+import MenuTitle from "./components/menu-title";
 
 export default function DashboardPage({
   children,
@@ -6,8 +7,11 @@ export default function DashboardPage({
   children: React.ReactNode;
 }) {
   return (
-    <div className="grid grid-cols-[250px_1fr] h-screen">
-      <MainMenu />
+    <div className="grid md:grid-cols-[250px_1fr] h-screen">
+      <MainMenu className="hidden md:flex" />
+      <div className="p-4 block md:hidden sticky top-0 left-0 bg-background border-b border-border">
+        <MenuTitle />
+      </div>
       <div className="overflow-auto py-2 px-4">
         <h1 className="pb-4">Welcome back, Tom!</h1>
         {children}
